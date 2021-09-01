d038b8a285248260c7440812180b2c2d6db1e561
Steve C <stevencarofano@gmail.com>
2021-09-01 13:01:43 -0400

    Updating file layout and routes

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const fs = require("fs");
 
 // Initialize the app and create a port
 const app = express();
@@ -9,9 +8,12 @@ const PORT = process.env.PORT || 3000;
 // Set up body parsing, static, and route middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")));
+
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "/public/notes.html")));
+
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
